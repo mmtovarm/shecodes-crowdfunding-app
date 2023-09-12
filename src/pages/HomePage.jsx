04553095a3ed1ee95afb419680046650497e077e
mmtovarm <monicamtovar@yahoo.com>
2023-09-12 18:18:54 +1000

    creating a repo for the crowdfunding app

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+
+import useProjects from "../hooks/useProjects";
+import ProjectCard from "../components/ProjectCard";
+import "./HomePage.css";
+function HomePage() {
+    const { projects } = useProjects();
+    console.log(projects)
+    return (
+        <div id="project-list">
+            {projects.map((projectData, key) => {
+                return <ProjectCard key={key} projectData={projectData} />;
+    
+            })}
+        </div>  
+    );
+}
+
+export default HomePage;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+import LoginForm from "../components/LoginForm";
+
+function LoginPage() {
+    return<h1>LoginForm</h1>
+}
+
+export default LoginPage;
