deca8e21aa9475ea2ead11401ab2b4d2899fea84
mmtovarm <monicamtovar@yahoo.com>
2023-09-16 13:13:23 +1000

    Login form changes

--- src/api/postLogin.js
+++ src/api/postLogin.js
@@ -1,6 +1,6 @@
 async function postLogin(username, password) {
     const url = `${import.meta.env.VITE_API_URL}/api-token-auth/`;
-    const response =awaitfetch(url,{
+    const response =await fetch(url,{
         method: "POST",
         headers: {
             "Content-type": "application/json",
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,12 @@
 import LoginForm from "../components/LoginForm";
 
 function LoginPage() {
-    return<h1>LoginForm</h1>
+    return (
+        <div>
+            <h1>LoginForm</h1>
+            <LoginForm />
+        </div>
+    )
 }
 
 export default LoginPage;
